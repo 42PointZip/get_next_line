72eea845c327537626df520418560e7581354e0c
42PointZip <lasalced@student.42.fr>
2014-12-06 12:54:53 +0100

    get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/28 10:39:50 by lasalced          #+#    #+#             */
-/*   Updated: 2014/12/01 14:37:39 by                  ###   ########.fr       */
+/*   Updated: 2014/12/03 20:30:04 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ int			ft_read(const int fd, char *buf, char **line)
 	int		ret;
 	static char	*str = 0;
 
-	ret = 1;
 	while (!(ft_strchr(str, '\n')) && (ret = read(fd, buf, BUFF_SIZE)))
-	{
-			buf[ret] = 0;
-			str = ft_strjoin(str, buf);
+	{	
+		if (ret == -1)
+			return (-1);
+		buf[ret] = '\0';
+		str = ft_strjoin(str, buf);
 	}
-	if (ft_strchr(str, '\n') && || (*line == ft_strdup(str)))
+	if (ft_strchr(str, '\n')  || ((*line == ft_strdup(str)) && 0))
 		*line = ft_strsub(str, 0, ft_strchr(str, '\n') - str + 1);
 	if (ret)
-		line[0][ft_strlen(*line) - 1] = 0;
+		line[0][ft_strlen(*line ) - 1] = 0;
 	str = ft_strsub(str, ft_strchr(str, '\n') - str + 1, \
 		   ft_strlen(ft_strchr(str, '\n')));
-	free (str);
 	if (ret == 0)
 		return (0);
 	else
